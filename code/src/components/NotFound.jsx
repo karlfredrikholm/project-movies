eb62ce175da92fe71410b49c69d76ff595e56338
Fredrik Holm <karlfredrikholm@gmail.com>
2022-10-14 14:04:08 +0200

    messing around with the routing

--- code/src/components/NotFound.jsx
+++ code/src/components/NotFound.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const NotFound = () => {
   return (
-    <h1 style={{ backgroundColor: 'red', color: 'white', fontSize: '2rem', width: '50%', height: '500px', margin: '10rem auto', paddingTop: '14rem', textAlign: 'center' }}>ERROR 404; Page not found</h1>
+    <div style={{ paddingTop: '10rem', height: '100vh', backgroundColor: 'black' }}>
+      <h1 style={{ backgroundColor: 'red', color: 'white', fontSize: '2rem', width: '50%', height: '500px', margin: '0 auto', paddingTop: '14rem', textAlign: 'center' }}>ERROR 404; Page not found</h1>
+    </div>
   )
 }
 
