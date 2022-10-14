82bd2b8b495c8e3da4c7a5aeba1f57aef187cc15
Fredrik Holm <karlfredrikholm@gmail.com>
2022-10-14 20:31:35 +0200

    media queries

--- code/src/components/Movie.jsx
+++ code/src/components/Movie.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Movie = ({ movie }) => {
   return (
-    <article className="movieContainer" style={{ backgroundImage: `url(https://image.tmdb.org/t/p/w342/${movie.poster_path})` }}>
+    <article className="movieContainer" style={{ backgroundImage: `url(https://image.tmdb.org/t/p/w500/${movie.poster_path})` }}>
       <div className="overlay">
         <h3 className="listTitle">{movie.title}</h3>
         <p className="releaseDate">Released {movie.release_date}</p>
